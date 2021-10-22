ac1bd9c07196a95f99ea2679175bf3ef877d679f
Amjad AL-Kadi <58593215+AmjadALKadi@users.noreply.github.com>
2021-10-22 06:33:52 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,15 @@
 
 
 makeCacheMatrix <- function(x = matrix()) {
-                    nel <- NULL                                    ## nel = null make var  = null
-                    pro <- function(a){                            ## that function to store tempo var in x 
-                      x <<- a 
-                      nel <<- NULL                                 ## make the var nel = null again
-                    }
-                    do <- function() {x}                           ## function to make x as input
-                    setinvrse <- function(inverse) {nel <<- inverse}
-                    getinvrse <- function() {nel}
-                    list(pro = pro , do = do , setinvrse =setinvrse , getinvrse=getinvrse)             ## make tempo list to store var x 
+                           nel <- NULL                                    ## nel = null make var  = null
+                           pro <- function(a){                            ## that function to store tempo var in x 
+                                          x <<- a 
+                                          nel <<- NULL                                 ## make the var nel = null again
+                                            }
+                           do <- function() {x}                           ## function to make x as input
+                           setinvrse <- function(inverse) {nel <<- inverse}
+                           getinvrse <- function() {nel}
+                           list(pro = pro , do = do , setinvrse =setinvrse , getinvrse=getinvrse)             ## make tempo list to store var x 
                     
 }
 
@@ -20,14 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
 #inverting the cached array and showing the result of the inverse of the array.
 
 cacheSolve <- function(x, ...) {
-                    nel <- x$getinvrse()
-                    if (!is.null(nel)) {
-                      message("get data")
-                      return(nel)
-                    }
-                    matx <- x$do()
-                    nel <- solve(matx , ...)
-                    x$setinvrse(nel)
-                    nel
+                          nel <- x$getinvrse()
+                          if (!is.null(nel)) {
+                                      message("get data")
+                                       return(nel)
+                                       }
+                          matx <- x$do()
+                          nel <- solve(matx , ...)
+                          x$setinvrse(nel)
+                          nel
                     
 }
